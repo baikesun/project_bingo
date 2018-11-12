d70dfb56c89003cc35145cd76e19b14f9523d1a3
baikesun <curious98@naver.com>
2018-11-13 00:48:25 +0900

    02

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 5
-//int형 아닐 수도 있음. ()안 변수선언으로 채우기 
 void initiate_bingo();   //빙고 테이블을 초기에 만들어줌 
 void print_bingo();   //빙고 테이블 현재 상황을 화면에 출력 
 void get_number_byMe();   //내가 빙고 번호 입력 선택 
@@ -12,29 +10,66 @@ void process_bingo();   //선택된 숫자를 입력받아서 빙고 테이블 칸을 채움
 void count_bingo();   //빙고 테이블이 채운 가로/세로/대각선 줄 수를 계산해서 반환
 void swap();   //선택된 숫자를 -1로 변경
 
- 
+int N;
+int M;
+
 int main()
 {
+	printf("N*N 사이즈의 빙고판을 만듭니다. N은? ");
+	scanf("%d", &N);
+	
+	printf("몇 줄의 빙고를 만들면 끝낼까요? ");
+	scanf("%d", &M);
+	
 	print_bingo(N, N);
 	
 	get_number_byMe(); 
 }
 
-void print_bingo(int i, int j)
+void print_bingo()
 {
-	int bingo[i][j];
-	int max = N*N;
-	
 	srand(time(NULL));
+	
+	int bingo[N][N];
+	int i, j, k, temp;
+	int count = 1;
+	
+	int rand_x = rand() % N;   //랜덤 x값 
+	int rand_y = rand() % N;   //랜덤 y값 
+	
+	
+	//배열에 1부터 N*N까지의 숫자를 넣음 
 	for(i=0; i<N; i++)
 	{
 		for(j=0; j<N; j++)
 		{
-			bingo[i][j] = 1 + (rand() % max);
-			printf("%d ", bingo[i][j]);
+			bingo[i][j] = count++;
+		}
+	 } 
+	
+	 //배열에 저장된 수를 랜덤하게 섞음(N번 반복) 
+	for(k=0; k<N; k++)
+	{
+		for(i=0; i<N; i++)
+		{
+	 		for(j=0; j<N; j++)
+	 		{
+	 			temp = bingo[i][j];
+	 			bingo[i][j] = bingo[rand_x][rand_y];
+	 			bingo[rand_x][rand_y] = temp;
+			}
+		}
+	}
+	
+
+	for(i=0; i<N; i++)
+	{
+	 	for(j=0; j<N; j++)
+	 	{
+	 		printf("%2d ", bingo[i][j]);
 		}
 		
-		printf("%d ", bingo[i][j]);
+		printf("\n");
 	}
 }
 
