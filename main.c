2bfbc8824da61250c7afa4c2806e8998b1261b44
baikesun <curious98@naver.com>
2018-11-13 15:12:52 +0900

    04

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define N 5
+#define M 2
+
 int initiate_bingo();   //빙고 테이블을 초기에 만들어줌 
 int print_bingo();   //빙고 테이블 현재 상황을 화면에 출력 
 void get_number_byMe();   //내가 빙고 번호 입력 선택 
@@ -10,16 +13,9 @@ void process_bingo();   //선택된 숫자를 입력받아서 빙고 테이블 칸을 채움
 void count_bingo();   //빙고 테이블이 채운 가로/세로/대각선 줄 수를 계산해서 반환
 void swap();   //선택된 숫자를 -1로 변경
 
-int N;
-int M;
-
 int main()
 {
-	printf("N*N 사이즈의 빙고판을 만듭니다. N은? ");
-	scanf("%d", &N);
-	
-	printf("몇 줄의 빙고를 만들면 끝낼까요? ");
-	scanf("%d", &M);
+	printf("<<사용자의 빙고판>>\n");
 	
 	initiate_bingo(N, N);
 	
@@ -69,17 +65,17 @@ int initiate_bingo()
 	 		printf("%2d ", bingo[i][j]);
 		}
 		
-		printf("\n");
+		printf("\n\n");
 	}
 	
 	return bingo[N][N];
 }
-
+/*
 void print_bingo()
 {
 	
 }
-
+*/
 void get_number_byMe(int num)   //num은 사용자가 선택한 숫자 
 {
 	int selected_num = -1;   //이미 선택된 숫자(-1로 표시)
@@ -91,7 +87,7 @@ void get_number_byMe(int num)   //num은 사용자가 선택한 숫자
 	{
 		swap(&num, &selected_num);
 		
-		print_bingo();		
+		// print_bingo();		
 	}
 	else
 	{
